22eedafad574028d38935d4df4784a58f861f0a8
guohaixia <1151472331@qq.com>
2024-07-10 15:38:43 +0800

    6、性格维度管理功能中的展示性格维度列表

---
+++ src/main/java/com/mbti/controller/DimensionServlet.java
@@ -0,0 +1,44 @@
+package com.mbti.controller;
+
+import com.mbti.entity.AssessmentType;
+import com.mbti.entity.PersonalityDimension;
+import com.mbti.service.AssessmentTypeService;
+import com.mbti.service.DimensionService;
+import com.mbti.service.impl.AssessmentTypeServiceImpl;
+import com.mbti.service.impl.DimensionServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 性格维度的控制器：处理跟性格维度相关的请求
+ */
+@WebServlet("/dimension")
+public class DimensionServlet extends HttpServlet {
+    DimensionService dimensionService=new DimensionServiceImpl();
+    AssessmentTypeService assessmentTypeService=new AssessmentTypeServiceImpl();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //根据不同的操作类型进行判断，分别进行不同的处理
+        String opr=req.getParameter("opr");
+        if("list".equals(opr)){//打开展示性格维度列表的页面：请求数据库，获取性格维度的所有记录，传递给前端页面
+            //调用业务逻辑层相关方法
+            List<PersonalityDimension> dimensionList=dimensionService.getDimensionList();
+            List<AssessmentType> assessmentTypeList=assessmentTypeService.getAssessmentTypeList();
+            //把列表 保存在reques对象中(性格维度、考核类型)
+            req.setAttribute("dimensionList",dimensionList);
+            req.setAttribute("assessmentList",assessmentTypeList);
+            req.getRequestDispatcher("WEB-INF/pages/dimension/list.jsp").forward(req,resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
---
+++ src/main/java/com/mbti/dao/AssessmentTypeDao.java
@@ -0,0 +1,9 @@
+package com.mbti.dao;
+
+import com.mbti.entity.AssessmentType;
+
+import java.util.List;
+
+public interface AssessmentTypeDao {
+    List<AssessmentType> getAssessmentTypeList();
+}
